6397207bcec385f0922cd8c4555fcf608cfef4f3
norma <normanmagic2004@gmail.com>
2025-05-17 20:07:19 +0300

    add diagrams

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { FaUserAlt } from "react-icons/fa";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { navLinks } from "../Sidebar/Sidebar";
